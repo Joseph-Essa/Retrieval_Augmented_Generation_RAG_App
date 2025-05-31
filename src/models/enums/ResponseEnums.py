11705d07b73d0d91025f4f11947ae0d1a3f5da55
Joseph-Essa <josepheissa0@gmail.com>
2025-05-31 23:10:57 +0300

    upload and process files to chunks

---
+++ src/models/enums/ResponseEnums.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class ResponseSignal(Enum):
+
+    FILE_TYPE_NOT_SUPPORTED = "File type not supported"
+    FILE_SIZE_EXCEEDED = "file size exceeded"
+    FILE_UPLOAD_SUCCESS = "file upload success"
+    FILE_UPLOAD_FAILED = "file upload failed"
+    FILE_VALIDATED_SUCCESS = "file validated success"
+    FILE_VALIDATED_FAILED = "file validated failed"
+    PROCESSING_SUCCESS = "processing success"
+    PROCESSING_FAILED = "processing failed"
+
+    
