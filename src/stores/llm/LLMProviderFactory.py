7de784ddeb59b252317c69075f869702339cefa7
Joseph-Essa <josepheissa0@gmail.com>
2025-06-06 01:38:23 +0300

    create LLM providers

---
+++ src/stores/llm/LLMProviderFactory.py
@@ -0,0 +1,31 @@
+from .LLMEnums import LLMEnum
+from .providers import OpenAIProvider , CoHereProvider
+
+class LLMProviderFactory:
+    def __init__(self , config : dict):
+        
+        self.config = config
+        
+    def create (self , provider : str) : 
+        
+        if provider == LLMEnum.GEMINI.value : 
+            return OpenAIProvider(
+                api_key = self.config.GEMINI_API_KEY ,
+                api_url = self.config.GEMINI_API_URL ,
+                default_input_max_characters = self.config.INPUT_DAFAULT_MAX_CHARACTERS , 
+                default_generation_max_output_tokens = self.config.GENERATION_DAFAULT_MAX_TOKENS , 
+                default_generation_temperature = self.config.GENERATION_DAFAULT_TEMPERATURE 
+            )
+        
+            
+        if provider == LLMEnum.COHERE.value : 
+            return CoHereProvider(
+                api_key = self.config.COHERE_API_KEY,
+                default_input_max_characters = self.config.INPUT_DAFAULT_MAX_CHARACTERS , 
+                default_generation_max_output_tokens = self.config.GENERATION_DAFAULT_MAX_TOKENS , 
+                default_generation_temperature = self.config.GENERATION_DAFAULT_TEMPERATURE 
+            )
+            
+        return None
+        
+            
