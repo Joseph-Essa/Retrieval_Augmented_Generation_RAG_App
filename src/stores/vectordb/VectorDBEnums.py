4bfaae44664886e4d719f39c561a632c153c0677
Joseph-Essa <josepheissa0@gmail.com>
2025-06-06 19:41:15 +0300

    create vector db enums

---
+++ src/stores/vectordb/VectorDBEnums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class VectorDBEnums(Enum) :
+     
+     QDRANT = "QDRANT"
+     
+class DistanceMethodEnum (Enum) : 
+    
+    COSINE = "cosine"
+    DOT = "dot"
+    
+    
