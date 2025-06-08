601c63a15ddce40dcf9cef48c4e444cc0595c362
Joseph-Essa <josepheissa0@gmail.com>
2025-06-09 02:36:39 +0300

    add template_parser object

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
 from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
+from stores.llm.templates.template_parser import TemplateParser
 
 
 app = FastAPI()
@@ -32,6 +33,12 @@ async def startup_span():
     app.vectordb_client = vectordb_provider_factory.create(provider=setting.VECTOR_DB_BACKEND)
      
     app.vectordb_client.connect() 
+    
+    app.template_parser = TemplateParser(
+        language=setting.PRIMARY_LANG ,
+        default_language = setting.DEFAULT_LANG   
+    )
+    
 
 # @app.on_event("shutdown")
 async def shutdown_span():
