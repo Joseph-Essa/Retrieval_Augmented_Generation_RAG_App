7220f873c0d44759ab1736088a3177ef505e2dea
Joseph-Essa <josepheissa0@gmail.com>
2025-06-09 02:40:42 +0300

    add LLm answer route

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -30,7 +30,8 @@ async def index_project (request : Request , project_id : str , push_request : P
         )
     nlp_controller = NLPController(vectordb_client = request.app.vectordb_client , 
                                    generation_client = request.app.generation_client , 
-                                   embedding_client = request.app.embedding_client
+                                   embedding_client = request.app.embedding_client ,
+                                   template_parser = request.app.template_parser
                                    )
     has_records = True
     page_no = 1
@@ -85,7 +86,8 @@ async def get_project_index_info (request : Request , project_id : str) :
     
     nlp_controller = NLPController(vectordb_client = request.app.vectordb_client , 
                                    generation_client = request.app.generation_client , 
-                                   embedding_client = request.app.embedding_client
+                                   embedding_client = request.app.embedding_client , 
+                                   template_parser = request.app.template_parser
                                    )
     
     collection_info = nlp_controller.get_vector_db_collection_info(project=project)
@@ -108,7 +110,8 @@ async def  search_index( request : Request , project_id : str ,  search_request 
     
     nlp_controller = NLPController(vectordb_client = request.app.vectordb_client , 
                                    generation_client = request.app.generation_client , 
-                                   embedding_client = request.app.embedding_client
+                                   embedding_client = request.app.embedding_client , 
+                                   template_parser = request.app.template_parser
                                    )
     
     results = nlp_controller.search_vector_db_collection(project=project , 
@@ -130,3 +133,39 @@ async def  search_index( request : Request , project_id : str ,  search_request 
         }
     )
     
+@nlp_router.post("/index/answer/{project_id}") 
+async def  search_index( request : Request , project_id : str ,  search_request : SearchRequest) : 
+    
+    project_model = await ProjectModel.create_instance(db_client = request.app.db_client)
+    project = await project_model.get_project_or_create_one(project_id=project_id)
+    
+    nlp_controller = NLPController(vectordb_client = request.app.vectordb_client , 
+                                   generation_client = request.app.generation_client , 
+                                   embedding_client = request.app.embedding_client , 
+                                   template_parser = request.app.template_parser
+                                   )
+
+    
+    answer , full_prompt , chat_history = nlp_controller.answer_rag_question(
+        project=project , 
+        query=search_request.text, 
+        limit=search_request.limit
+    )
+    
+    if not answer : 
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content = {
+                "signal" : ResponseSignal.RAG_ANSWER_ERROR.value ,
+            }
+        )
+    return JSONResponse(
+        content = {
+            "signal" : ResponseSignal.RAG_ANSWER_SUCCESS.value ,
+            "answer" : answer , 
+            "full_prompt" : full_prompt , 
+            "chat_history" : chat_history
+        }
+    )
+    
+
